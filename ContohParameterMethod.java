0b68bfef155cf54fe5c39b5b9cba9f22d13643b3
2382028 <2382028@unai.edu>
2024-09-24 10:10:52 +0700

    handson4

---
+++ ContohParameterMethod.java
@@ -0,0 +1,18 @@
+public class ContohParameterMethod {
+    public static void kaliDua(int angka) {
+        int hasil = angka * 2;
+        return hasil;
+    }
+
+    public static double hitungLuasSegitiga(double alas, double tinggi) {
+      return 0.5 * alas * tinggi;
+    }
+
+    public static void main(String[] args) {
+        int hasilKaliDua = kaliDua(5);
+        System.out.println("5 x 2 = "+ hasilKaliDua);
+
+        double luasSegitiga = hitungLuasSegitiga(4, 6);
+        System.out.println();
+    }
+}
